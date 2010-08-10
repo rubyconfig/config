d7441bba379149775df481a769ee077891da9743
Jacques Crocker <railsjedi@gmail.com>
2010-08-10 16:24:25 -0700

    Settings should load before initializers

--- lib/rails_config/railtie.rb
+++ lib/rails_config/railtie.rb
@@ -1,13 +1,15 @@
 if defined?(Rails::Railtie)
   module RailsConfig
     class Railtie < Rails::Railtie
-      initializer :setup_rails_config do
+
+      ActiveSupport.on_load :before_initialize, :yield => true do
         ::Settings = RailsConfig.load_files(
           Rails.root.join("config", "settings.yml").to_s,
           Rails.root.join("config", "settings", "#{Rails.env}.yml").to_s,
           Rails.root.join("config", "environments", "#{Rails.env}.yml").to_s
         )
       end
+
     end
   end
 end
