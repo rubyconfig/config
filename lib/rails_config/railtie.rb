46d6a24a856d77fe6be57c0f1c268919abd59dd2
Jacques Crocker <railsjedi@gmail.com>
2010-08-10 16:44:58 -0700

    In Rails development mode, reload the config before each request

--- lib/rails_config/railtie.rb
+++ lib/rails_config/railtie.rb
@@ -2,12 +2,22 @@ if defined?(Rails::Railtie)
   module RailsConfig
     class Railtie < Rails::Railtie
 
+      # Parse the settings before any of the initializers
       ActiveSupport.on_load :before_initialize, :yield => true do
         ::Settings = RailsConfig.load_files(
           Rails.root.join("config", "settings.yml").to_s,
           Rails.root.join("config", "settings", "#{Rails.env}.yml").to_s,
           Rails.root.join("config", "environments", "#{Rails.env}.yml").to_s
         )
+      end
+
+      # Rails Dev environment should reload the Settings on every request
+      if Rails.env.development?
+        initializer :rails_config_reload_on_development do
+          ActionController::Base.class_eval do
+            prepend_before_filter { ::Settings.reload! }
+          end
+        end
       end
 
     end
