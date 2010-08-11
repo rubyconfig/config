3224a01cf6cdd300d2f5638c2c827ba00bbc270b
Jacques Crocker <railsjedi@gmail.com>
2010-08-10 17:16:13 -0700

    Add the ability to define the settings object via config.rails_config.constant

--- lib/rails_config/railtie.rb
+++ lib/rails_config/railtie.rb
@@ -1,14 +1,18 @@
 if defined?(Rails::Railtie)
   module RailsConfig
     class Railtie < Rails::Railtie
+      config.rails_config = ActiveSupport::OrderedOptions.new
 
       # Parse the settings before any of the initializers
       ActiveSupport.on_load :before_initialize, :yield => true do
-        ::Settings = RailsConfig.load_files(
+        settings = RailsConfig.load_files(
           Rails.root.join("config", "settings.yml").to_s,
           Rails.root.join("config", "settings", "#{Rails.env}.yml").to_s,
           Rails.root.join("config", "environments", "#{Rails.env}.yml").to_s
         )
+
+        # setup the constant
+        Kernel.const_set(app.config.rails_config.constant || "Settings", settings)
       end
 
       # Rails Dev environment should reload the Settings on every request
