1180e442c264740d399dd255867ffd5d4738f98f
Neer Sighted <neersighted@myopera.com>
2012-12-27 15:15:43 -0800

    Try something new

--- lib/rails_config/integration/rails.rb
+++ lib/rails_config/integration/rails.rb
@@ -4,31 +4,27 @@ module RailsConfig
       if defined?(Rails::Railtie)
         class Railtie < Rails::Railtie
 
-          # manually load the custom initializer before everything else
-          initializer :load_custom_rails_config, :before => :load_environment_config, :group => :all do
-            initializer = Rails.root.join("config", "initializers", "rails_config.rb")
-            require initializer if File.exist?(initializer)
-          end
+          initializer "rails_config" do
+            ActiveSupport.on_load :before_configuration, :yield => true do
+              initializer = Rails.root.join("config", "initializers", "rails_config.rb")
+              require initializer if File.exist?(initializer)
 
-          # Parse the settings before any of the initializers
-          #initializer :load_rails_config_settings, :after => :load_custom_rails_config, :before => :load_environment_config, :group => :all do
-          ActiveSupport.on_load :before_configuration, :yield => true do
-            RailsConfig.load_and_set_settings(
-              Rails.root.join("config", "settings.yml").to_s,
-              Rails.root.join("config", "settings", "#{Rails.env}.yml").to_s,
-              Rails.root.join("config", "environments", "#{Rails.env}.yml").to_s,
 
-              Rails.root.join("config", "settings.local.yml").to_s,
-              Rails.root.join("config", "settings", "#{Rails.env}.local.yml").to_s,
-              Rails.root.join("config", "environments", "#{Rails.env}.local.yml").to_s
-            )
-          end
+              RailsConfig.load_and_set_settings(
+                Rails.root.join("config", "settings.yml").to_s,
+                Rails.root.join("config", "settings", "#{Rails.env}.yml").to_s,
+                Rails.root.join("config", "environments", "#{Rails.env}.yml").to_s,
 
-          # Rails Dev environment should reload the Settings on every request
-          if Rails.env.development?
-            initializer :rails_config_reload_on_development do
-              ActionController::Base.class_eval do
-                prepend_before_filter { ::RailsConfig.reload! }
+                Rails.root.join("config", "settings.local.yml").to_s,
+                Rails.root.join("config", "settings", "#{Rails.env}.local.yml").to_s,
+                Rails.root.join("config", "environments", "#{Rails.env}.local.yml").to_s
+              )
+              
+              # Rails Dev environment should reload the Settings on every request
+              if Rails.env.development?
+                ActionController::Base.class_eval do
+                  prepend_before_filter { ::RailsConfig.reload! }
+                end
               end
             end
           end
