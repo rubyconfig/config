a21fe335b3e95ef166e2a97ed5fc5263231f913e
Piotr Kuczynski <piotr.kuczynski@here.com>
2014-03-26 16:57:59 +0100

    Fixes #59, #50, #44 and #32

--- lib/rails_config/integration/rails.rb
+++ lib/rails_config/integration/rails.rb
@@ -3,15 +3,12 @@ module RailsConfig
     module Rails3
       if defined?(Rails::Railtie)
         class Railtie < Rails::Railtie
-
-          # manually load the custom initializer before everything else
-          initializer :load_custom_rails_config, :before => :load_environment_config, :group => :all do
+          ActiveSupport.on_load :before_configuration, :yield => true do
+            # manually load the custom initializer before everything else
             initializer = Rails.root.join("config", "initializers", "rails_config.rb")
             require initializer if File.exist?(initializer)
-          end
 
-          # Parse the settings before any of the initializers
-          initializer :load_rails_config_settings, :after => :load_custom_rails_config, :before => :load_environment_config, :group => :all do
+            # Parse the settings before any of the initializers
             RailsConfig.load_and_set_settings(
               Rails.root.join("config", "settings.yml").to_s,
               Rails.root.join("config", "settings", "#{Rails.env}.yml").to_s,
