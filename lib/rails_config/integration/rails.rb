815c7c4c55243565bf262e79292f473744980234
Jacques Crocker <railsjedi@gmail.com>
2014-04-05 14:25:32 -0700

    fully qualify rails module path

--- lib/rails_config/integration/rails.rb
+++ lib/rails_config/integration/rails.rb
@@ -1,11 +1,11 @@
 module RailsConfig
   module Integration
     module Rails
-      if defined?(Rails::Railtie)
-        class Railtie < Rails::Railtie
+      if defined?(::Rails::Railtie)
+        class Railtie < ::Rails::Railtie
 
           # TODO: allo them to override init_callback via ENV or something?
-          if Rails::VERSION::MAJOR >= 4 and Rails::VERSION::MINOR >= 1
+          if ::Rails::VERSION::MAJOR >= 4 and ::Rails::VERSION::MINOR >= 1
             init_callback = :before_initialize
           else
             init_callback = :before_configuration
@@ -13,23 +13,23 @@ module RailsConfig
 
           ActiveSupport.on_load init_callback, :yield => true do
             # manually load the custom initializer before everything else
-            initializer = Rails.root.join("config", "initializers", "rails_config.rb")
+            initializer = ::Rails.root.join("config", "initializers", "rails_config.rb")
             require initializer if File.exist?(initializer)
 
             # Parse the settings before any of the initializers
             RailsConfig.load_and_set_settings(
-              Rails.root.join("config", "settings.yml").to_s,
-              Rails.root.join("config", "settings", "#{Rails.env}.yml").to_s,
-              Rails.root.join("config", "environments", "#{Rails.env}.yml").to_s,
+              ::Rails.root.join("config", "settings.yml").to_s,
+              ::Rails.root.join("config", "settings", "#{::Rails.env}.yml").to_s,
+              ::Rails.root.join("config", "environments", "#{::Rails.env}.yml").to_s,
 
-              Rails.root.join("config", "settings.local.yml").to_s,
-              Rails.root.join("config", "settings", "#{Rails.env}.local.yml").to_s,
-              Rails.root.join("config", "environments", "#{Rails.env}.local.yml").to_s
+              ::Rails.root.join("config", "settings.local.yml").to_s,
+              ::Rails.root.join("config", "settings", "#{::Rails.env}.local.yml").to_s,
+              ::Rails.root.join("config", "environments", "#{::Rails.env}.local.yml").to_s
             )
           end
 
           # Rails Dev environment should reload the Settings on every request
-          if Rails.env.development?
+          if ::Rails.env.development?
             initializer :rails_config_reload_on_development do
               ActionController::Base.class_eval do
                 prepend_before_filter { ::RailsConfig.reload! }
