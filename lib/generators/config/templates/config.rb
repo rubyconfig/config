d2da86883f14331332b375167568b3ac4b2942e2
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2016-01-03 15:06:00 +0100

    Added knockout_prefix to rails initializer

--- lib/generators/config/templates/config.rb
+++ lib/generators/config/templates/config.rb
@@ -2,4 +2,6 @@ Config.setup do |config|
   # Name of the constat exposing loaded settings
   config.const_name = 'Settings'
 
+  # Ability to remove elements of the array set in earlier loaded settings file. Default: nil
+  # config.knockout_prefix = '--'
 end
