4451f15d3fdd1fc6ef0b4b89e7839772171f1e23
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2016-03-26 00:47:06 +0100

    Fix reek integration into Rspec

--- spec/reek_spec.rb
+++ spec/reek_spec.rb
@@ -4,9 +4,9 @@ require 'spec_helper'
 
 describe 'Make sure source code' do
   # TODO: enable this test once all reek warnings are fixed and https://github.com/troessner/reek/issues/895 is solved
-  # it 'does not contain any smells' do
-  #   Dir['lib/**/*.rb'].each do |file|
-  #     expect(file).not_to reek
-  #   end
-  # end
+  it 'does not contain any smells' do
+    Pathname.glob('lib/**/*.rb').each do |file|
+      expect(file).not_to reek
+    end
+  end
 end
