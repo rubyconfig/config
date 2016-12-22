26193e869aba44a0761e5adb62ea21d0a21c20cb
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2016-12-22 02:25:14 +0100

    Exclude validation tests for older version of Ruby

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -1,37 +1,39 @@
 require 'spec_helper'
 
-describe Config do
-  context 'validation' do
-    around(:each) do |example|
-      Config.reset
-      example.run
-      Config.reset
-    end
-
-    it 'should raise if schema is present and validation fails' do
-      Config.setup do |config|
-        config.schema do
-          required(:youtube).schema do
-            required(:nonexist_field).filled
-          end
-        end
+if RUBY_VERSION >= '2.1'
+  describe Config do
+    context 'validation' do
+      around(:each) do |example|
+        Config.reset
+        example.run
+        Config.reset
       end
 
-      expect { Config.load_files("#{fixture_path}/validation/config.yml") }.
-        to raise_error(Config::Validation::Error, /youtube.nonexist_field: is missing/)
-    end
-
-    it 'should work if validation passes' do
-      Config.setup do |config|
-        config.schema do
-          required(:youtube).schema do
-            required(:api_key).filled
+      it 'should raise if schema is present and validation fails' do
+        Config.setup do |config|
+          config.schema do
+            required(:youtube).schema do
+              required(:nonexist_field).filled
+            end
           end
         end
+
+        expect { Config.load_files("#{fixture_path}/validation/config.yml") }.
+          to raise_error(Config::Validation::Error, /youtube.nonexist_field: is missing/)
       end
 
-      expect { Config.load_files("#{fixture_path}/validation/config.yml") }.
-        to_not raise_error
+      it 'should work if validation passes' do
+        Config.setup do |config|
+          config.schema do
+            required(:youtube).schema do
+              required(:api_key).filled
+            end
+          end
+        end
+
+        expect { Config.load_files("#{fixture_path}/validation/config.yml") }.
+          to_not raise_error
+      end
     end
   end
 end
