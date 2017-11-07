d48d27d97de1a90152a80a3fd189c21b54c5dd8d
Yuki Hattori <yuki.hattori@speee.jp>
2017-11-07 11:45:58 +0900

    Add testing by dummy sinatra app

--- spec/tasks/db_spec.rb
+++ spec/tasks/db_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
 
-describe 'db:create' do
-  include_context 'rake'
+describe 'db:create', :rails do
+  before do
+    require 'rake'
+    load 'rails/tasks/engine.rake'
+  end
 
   it 'has access to Settings object and can read databases from settings.yml file' do
     Rake::Task['db:create'].invoke
