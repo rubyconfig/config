0dbdd680c2134d9d80ad6ffb9f04a101a67ae20f
Ryan Jackson <ryanwjackson@gmail.com>
2019-07-16 11:34:42 -0400

    Add dry-validation contract support

--- lib/config/validation/validate.rb
+++ lib/config/validation/validate.rb
@@ -3,18 +3,21 @@ require 'config/validation/error'
 module Config
   module Validation
     module Validate
-
       def validate!
-        if Config.schema
-          v_res = Config.schema.(self.to_hash)
-
-          unless v_res.success?
-            error = Config::Validation::Error.format(v_res)
-            raise Config::Validation::Error.new("Config validation failed:\n\n#{error}")
-          end
-        end
+        validate_using!(data: to_hash, schema: Config.validation_contract) if Config.validation_contract
+        validate_using!(data: to_hash, schema: Config.schema) if Config.schema
       end
 
+      private
+
+      def validate_using!(data:, schema:)
+        v_res = schema.(data)
+
+        return if v_res.success?
+
+        error = Config::Validation::Error.format(v_res)
+        raise Config::Validation::Error, "Config validation failed:\n\n#{error}"
+      end
     end
   end
 end
