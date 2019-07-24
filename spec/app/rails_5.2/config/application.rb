c7bada7eb1509b654456fdd8bb3fdfd4e84c5089
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2019-07-24 18:09:41 +0200

    X

--- spec/app/rails_5.2/config/application.rb
+++ spec/app/rails_5.2/config/application.rb
@@ -15,5 +15,10 @@ module Rails52
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    ##
+    # Config
+    #
+    config_available?
   end
 end
