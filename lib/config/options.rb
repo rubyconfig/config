b237c223f2159f326f5ba2eae23c4dfc3e2dc363
Chris LaRose <cjlarose@gmail.com>
2020-11-25 12:01:27 -0800

    Add exit! to reserved settings names
    Calling `Config::Options#[]` with the parameter `'exit!'` will invoke
    `Kernel#exit!` and immediately terminate the program. Instead, we want
    to handle this key explicitly, not because it's likely to be used as a
    key in a legitimate config yaml file, but to prevent a potential DOS
    vector if accessing options with untrusted input.

--- lib/config/options.rb
+++ lib/config/options.rb
@@ -145,7 +145,7 @@ module Config
     end
 
     # Some keywords that don't play nicely with OpenStruct
-    SETTINGS_RESERVED_NAMES = %w[select collect test count zip min max].freeze
+    SETTINGS_RESERVED_NAMES = %w[select collect test count zip min max exit!].freeze
 
     # An alternative mechanism for property access.
     # This let's you do foo['bar'] along with foo.bar.
