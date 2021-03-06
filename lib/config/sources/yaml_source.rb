567d4f1c2d152cd3424861df053ed2f2dec9eb7e
Wes Boynton <wes@boynton.io>
2021-03-06 17:13:49 -0500

    Add namespaces to YAML sources per #288

--- lib/config/sources/yaml_source.rb
+++ lib/config/sources/yaml_source.rb
@@ -6,15 +6,26 @@ module Config
     class YAMLSource
       attr_accessor :path
 
-      def initialize(path)
+      def initialize(path, namespace = nil)
         @path = path.to_s
+
+        # Make sure @namespace is an array if it exists at all
+        if namespace
+          @namespace = namespace
+          @namespace = [@namespace] unless @namespace.is_a?(Array)
+        end
       end
 
       # returns a config hash from the YML file
       def load
         result = YAML.load(ERB.new(IO.read(@path)).result) if @path and File.exist?(@path)
-
-        result || {}
+        return {} unless result
+        return result unless @namespace
+        
+        # Resolves namespacing multiple layers deep 
+        # i.e. ['layer1', 'layer2'] comes out to {'layer1' => {'layer2' => content}}
+        return  result = @namespace.reverse.inject(result) { |a, n| { n => a } }
+        
 
         rescue Psych::SyntaxError => e
           raise "YAML syntax error occurred while parsing #{@path}. " \
