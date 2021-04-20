5310f023f5b0ca0486a9dd1661f03273ea2f23e5
Gareth Griffiths <gareth.griffiths2@dvla.gov.uk>
2021-04-20 13:15:24 +0100

    config.rb - An initialiser added to override the default behaviour from Rails in terms of environments

---
+++ spec/app/rails_5.0/config/initializers/config.rb
@@ -0,0 +1,8 @@
+# This is how you would initialize the config object
+# when not using the default rails environments
+
+Config.setup do |config|
+  config.environment = ENV['NON_RAILS_ENVIRONMENT'].downcase
+  config.const_name = 'Settings'
+  config.env_parse_values = true
+end
