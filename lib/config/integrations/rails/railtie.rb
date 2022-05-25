4d313d2d7f3fcfaefc19305395a6ae324e0ab856
Neil Williams <neil.williams@dvla.gov.uk>
2022-05-25 09:41:42 +0100

    Added a new config option called 'environment', which allows you to overide the default config environment which is the Rails.env. This allows us to have multiple different environments, without having to worry about making custom rails environments

--- lib/config/integrations/rails/railtie.rb
+++ lib/config/integrations/rails/railtie.rb
@@ -9,7 +9,7 @@ module Config
 
           # Parse the settings before any of the initializers
           Config.load_and_set_settings(
-            Config.setting_files(::Rails.root.join('config'), ::Rails.env)
+            Config.setting_files(::Rails.root.join('config'), Config.environment.nil? ? ::Rails.env : Config.environment.to_sym)
           )
         end
 
