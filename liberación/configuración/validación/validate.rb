26ac8525bfa97d8ee83b11d65df42519ff395a33
fxckyuo <129841631+fxckyuo@users.noreply.github.com>
2023-04-04 04:39:06 -0600

    Update and rename lib/config/validation/validate.rb to liberación/configuración/validación/validate.rb

--- liberación/configuración/validación/validate.rb
+++ liberación/configuración/validación/validate.rb
@@ -6,17 +6,10 @@ module Config
       def validate!
         validate_using!(Config.validation_contract)
         validate_using!(Config.schema)
-      end
-
-      private
-
       def validate_using!(validator)
         if validator
           result = validator.call(to_hash)
 
-          return if result.success?
-
-          error = Config::Validation::Error.format(result)
           raise Config::Validation::Error, "Config validation failed:\n\n#{error}"
         end
       end
