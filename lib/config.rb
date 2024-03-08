2eec9580de39d04278e651f3db6f98b481bcb8fd
Alexander Dischberg <noxasch@gmail.com>
2024-03-09 04:09:12 +0800

    feat: add rails crendentails support

--- lib/config.rb
+++ lib/config.rb
@@ -30,6 +30,7 @@ module Config
     validation_contract: nil,
     evaluate_erb_in_yaml: true,
     environment: nil
+    use_rails_credentials: false
   )
 
   def self.setup
@@ -48,6 +49,10 @@ module Config
     end
 
     config.add_source!(Sources::EnvSource.new(ENV)) if Config.use_env
+
+    if defined?(::Rails::Railtie) && Config.use_rails_credentials
+      config.add_source!(Rails.application.credentials.to_h.deep_stringify_keys) 
+    end
 
     config.load!
     config
